7301997b6db70f6e39df5081b129e976df56bc28
Ben Greer <benjamin.greer@colorado.edu>
2015-05-07 11:14:45 -0600

    Commenting, formatting

--- TESTS/remotecontrol/run.sh
+++ TESTS/remotecontrol/run.sh
@@ -2,22 +2,28 @@
 
 DIR=/home/ubuntu/PROJECTS/HEX/TESTS/remotecontrol
 
+# clear previous log file
 rm -f ${DIR}/log
 
 tries=1
 running=0
 done=0
 
-rm -f ${DIR}/log
+
 while [ ${done} -ne 1 ]; do
+	# launch program in background
 	${DIR}/remote > ${DIR}/log &
 	tries=$((${tries}+1))
-	# wait for exit
-	running=`jobs | grep remote | wc | awk '{print $1}'`
+	# look to see if the program is running
+	# this command lists background jobs, filters for ones that are running, then counts them
+	# if the program is running, returns 1, else returns 0
+	running=`jobs | grep Running | wc | awk '{print $1}'`
+	# wait until it disappears
 	while [ ${running} -eq 1 ]; do
 		sleep 1
 		running=`jobs | grep Running | wc | awk '{print $1}'`
 	done
+	# if we've tried too many times, stop
 	if [ ${tries} -gt 25 ]
 	then
 		done=1
